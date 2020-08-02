a1bc00aa95c3bc0af30efb7ab22732eab69c3a8f
ysherqawi <ysherqawi@gmail.com>
2020-08-02 14:06:51 +0300

    edited addCategory comment

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -3,7 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Create category
 // @route   POST /api/v1/auth/category
-// @access  Public
+// @access  Private / admin
 exports.addCategory = async (req, res, next) => {
   const category = await Category.create(req.body);
   await category.save();
