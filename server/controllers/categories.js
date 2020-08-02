3c68b1b553435837293a5d6e3a9ccc944b049cab
ysherqawi <ysherqawi@gmail.com>
2020-08-02 23:07:40 +0300

    added categoryById middleware

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -1,6 +1,23 @@
 const Category = require('../models/Category');
 const ErrorResponse = require('../utils/errorResponse');
 
+// @desc    categoryById middleware
+// run whenever an id param found in the route and populate the category in the req
+exports.categoryById = async (req, res, next, id) => {
+  const category = await Category.findById(id);
+
+  if (!category)
+    return next(
+      new ErrorResponse(
+        `No Category found with the id of ${req.params.id}`,
+        404
+      )
+    );
+  req.category = category;
+
+  next();
+};
+
 // @desc    Create category
 // @route   POST /api/v1/categories
 // @access  Private / admin
--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { addCategory } = require('../controllers/categories');
+const { addCategory, categoryById } = require('../controllers/categories');
 
 const { protect, authorize } = require('../middleware/auth');
 router.route('/').post(protect, authorize('admin'), addCategory);
+
+router.param('id', categoryById);
 
 module.exports = router;
